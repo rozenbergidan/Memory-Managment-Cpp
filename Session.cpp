dc9fb08fe78db84536379e05d8bb52ab685a0fb3
idan <idanroze@post.bgu.ac.il>
2020-11-03 14:33:07 +0200

    started sessions

--- Session.cpp
+++ Session.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Session.h"
+using namespace  std;
+
+Session::Session(const std::string &path) : g(nullptr), treeType(nullptr), agents{
+
+};
+
+void addAgent(const Agent& agent){
+
+};
+void setGraph(const Graph& graph){
+
+};
