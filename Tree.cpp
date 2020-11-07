b3d5e4dc457f7d6dc028398757fc72d829416607
tsuri <tsurif@post.bgu.ac.il>
2020-11-07 15:58:38 +0200

    trees traceTree function written

--- Tree.cpp
+++ Tree.cpp
@@ -8,5 +8,112 @@ using namespace std;
 
 Tree::Tree(int rootLabel) : node(rootLabel){}
 
-void Tree::addChild(const Tree &child) {}
+void Tree::addChild(const Tree &child) {
+    //children.push_back((Tree*)&child);
+}
+int Tree::getNode() const {
+    return node;
+}
+
+
+Tree::~Tree(){
+    for(int i = 0; i < children.size(); i = i + 1){
+        if(children[i]) delete children[i];
+    }
+}
+//---------------------------------------CycleTree--------------------------------------------------------------
+const CycleTree& CycleTree::operator=(const CycleTree &other) {
+    children = other.children;
+    setNode(other.getNode());
+    currCycle = other.currCycle;
+    return *this;
+
+}
+
+int CycleTree::traceTree() {
+    CycleTree* outputTree = this;
+    for(int i = 0 ; i <= currCycle; i = i + 1){
+        outputTree = (CycleTree*)outputTree->children[0];
+    }
+}
+//---------------------------------------MaxRankTree------------------------------------------------------------
+
+int MaxRankTree::traceTree() {
+    MaxRankTree* max = this;
+    MaxRankTree* maxInchilds = traceTreeRecursion(children.size());
+    if(maxInchilds != nullptr) max = maxInchilds;
+    return max->node;
+}
+
+MaxRankTree* MaxRankTree::traceTreeRecursion(int currMax) {
+    MaxRankTree* output = nullptr;
+    if(children.size()>currMax) {
+        currMax = children.size();
+        output = this;
+    }
+    for(int i = 0; i < children.size();  i = i + 1){
+        MaxRankTree* maxInChild = ((MaxRankTree*)children[i])->traceTreeRecursion(currMax);
+        if (maxInChild != nullptr){
+            output = maxInChild;
+            currMax = maxInChild->node;
+        }
+    }
+    return output;
+}
+//---------------------------------------RootTree---------------------------------------------------------------
+int RootTree::traceTree() {
+    return getNode();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//std::vector<Tree*>* Tree::cloneChildren() const {
+//    std::vector<Tree*>* output = new vector<Tree*>(children);
+//    return output;
+//}
+//
+//CycleTree* CycleTree::clone() const{
+//    CycleTree* output = new CycleTree(this->getNode(),this->currCycle);
+//    output->setChildren(this->cloneChildren());
+//    return output;
+//}
+//
+//MaxRankTree* MaxRankTree::clone() const{
+//    MaxRankTree* output = new MaxRankTree(this->getNode());
+//    output->setChildren(this->cloneChildren());
+//    return output;
+//}
+
+//RootTree* RootTree::clone() const{
+//    RootTree* output = new RootTree(this->getNode());
+//    output->setChildren(this->cloneChildren());
+//    return output;
+//}
+
+
 
--- Tree.h
+++ Tree.h
@@ -10,18 +10,34 @@ public:
     Tree(int rootLabel);
     void addChild(const Tree& child);
 
+    //my code
+    virtual ~Tree();
+    virtual const Tree& operator=(const Tree& other);//copy assignment operator
+
+    int getNode() const;
+    void setNode(int n);
+
 
     static Tree* createTree(const Session& session, int rootLabel);
     virtual int traceTree()=0;
-private:
-    int node;
+
+protected:
     std::vector<Tree*> children;
+    int node;
+
 };
 
 class CycleTree: public Tree{
 public:
     CycleTree(int rootLabel, int currCycle);
     virtual int traceTree();
+
+    //my code
+    CycleTree(const CycleTree &other); //copy constructor
+    virtual ~CycleTree();// destructor
+    virtual const CycleTree& operator=(const CycleTree& other);//copy assignment operator
+
+
 private:
     int currCycle;
 };
@@ -30,12 +46,27 @@ class MaxRankTree: public Tree{
 public:
     MaxRankTree(int rootLabel);
     virtual int traceTree();
+
+    //my code
+    MaxRankTree(const MaxRankTree &other); //copy constructor
+    virtual ~MaxRankTree();// destructor
+    virtual const MaxRankTree& operator=(const MaxRankTree& other);//copy assignment operator
+private:
+    MaxRankTree* traceTreeRecursion(int currMax);// returns the node with most childrens that biger then currMax if exsist, and nullptr is not
 };
 
 class RootTree: public Tree{
 public:
     RootTree(int rootLabel);
     virtual int traceTree();
+
+    //my code
+    RootTree(const RootTree &other); //copy constructor
+    virtual ~RootTree();// destructor
+    virtual const RootTree& operator=(const RootTree& other);//copy assignment operator
+
+
+//    virtual RootTree* clone() const;
 };
 
 #endif
